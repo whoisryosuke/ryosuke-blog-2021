4b238e974e095da4f11350ef4342a237548d7448
Ryosuke <ryosuke.san.hana@gmail.com>
2021-02-15 21:08:36 -0800

    :bug: fix react specific lint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   parser: `@typescript-eslint/parser`,
   extends: [
     "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
     "plugin:prettier/recommended",
     "prettier/@typescript-eslint",
   ],
