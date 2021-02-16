4a116f686c0525c632bcd544cf4ea379c8c49740
Ryosuke <ryosuke.san.hana@gmail.com>
2021-02-15 20:17:00 -0800

    :tada: gatsby mdx styled typescript

---
+++ README.md
@@ -0,0 +1,28 @@
+# Personal Portfolio and Blog of Ryosuke
+
+## Post Format / Fields
+
+```markdown
+---
+title: Deploy a Static React Blog using GatsbyJS and Github
+date: "2018-03-21"
+section: blog
+cover_image: "./bulma-css-framework@1x.jpg"
+tags:
+  [
+    "design",
+    "development",
+    "react",
+    "github",
+    "gatsbyjs",
+    "ssg",
+    "static site generator",
+  ]
+---
+
+Your post here
+```
+
+- Section can be `blog` or `project`.
+- Tags must be array
+- Body content can include Markdown or HTML.
---
+++ gatsby-config.js
@@ -0,0 +1,107 @@
+module.exports = {
+  siteMetadata: {
+    title: `Ryosuke`,
+    description: `The portfolio and blog of Ryosuke Hana`,
+    author: `@whoisryosuke`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-mdx-embed`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: {
+          default: require.resolve(`./src/components/layout.js`),
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    {
+      resolve: `gatsby-plugin-feed-mdx`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.edges.map((edge) => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + `/blog` + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + `/blog` + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMdx(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: `/rss.xml`,
+            title: `Your Site's RSS Feed`,
+            // optional configuration to insert feed reference in pages:
+            // if `string` is used, it will be used to create RegExp and then test if pathname of
+            // current page satisfied this regular expression;
+            // if not provided or `undefined`, all pages will have feed reference inserted
+            match: `^/blog/`,
+          },
+        ],
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
